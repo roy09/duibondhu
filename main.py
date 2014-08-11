52788544d35559ef9e62a5daa629715e9e06e45a
roy09 <nextgen.ancestor@gmail.com>
2014-08-11 13:12:00 +0600

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 class NotFoundPageHandler(webapp.RequestHandler):
     def get(self):
         self.error(404)
-        self.response.out.write('<Your 404 error html page>')
+        self.response.out.write("lal pasa, man")
 
 application = webapp.WSGIApplication([('/.*', NotFoundPageHandler)],
                                      debug=True)
